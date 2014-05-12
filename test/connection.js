03b6b991a411a9a8887630efcaa4b9a3431dfc52
Daniel Pigott <pigottd@gmail.com>
2014-05-12 20:46:26 +1000

    Implementing more functions

--- test/connection.js
+++ test/connection.js
@@ -3,10 +3,14 @@ var Xantrex = require("../lib/xantrex.js").Xantrex;
 var xantrex = new Xantrex("/dev/ttyUSB0", 9600);
 xantrex.connect().then(
 function() {
-  xantrex.getKwhToday().then(
+  xantrex.getTemperature().then(
   function(kwh) {
     console.log(kwh);
-    xantrex.disconnect();});
+    xantrex.getKwhSummary().then(
+      function(kwh) {
+        console.log(kwh);
+        xantrex.disconnect();});
+  });
 });
 
 
